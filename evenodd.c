e20deaff6d00d483356889ba3d7a6a2f26c0134c
Vishnupriya24 <sailashmivishnu@gmail.com>
2016-09-14 22:07:28 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -4,11 +4,13 @@ int main(void) {
 	// your code goes here
 	int n;
 	scanf("%d\n",&n);
-	if(n>0)
-	{
+    if(n>0)
+     {
 	if((n%2)==0)
 	 printf("the number is even");
 	else 
 	 printf("the number is odd");
-	 }return 0;
+	 }else
+        printf("plz enter a positive number");
+      return 0;
 }
